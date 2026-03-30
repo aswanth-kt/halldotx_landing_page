23de549fcfea0a7509885ba98671a4af6f1f96d3
aswanthkt31 <aswanthkt99@gmail.com>
2026-03-30 23:08:48 +0530

    - Set up React project using Vite - Installed and configured Tailwind CSS (v3) - Created basic project folder structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-500">
+      Hall Dotx
+    </h1>
+    </>
+  )
+}
+
+export default App
